a4be9b7467dbcfac24372dc8402a136f2f1e579a
TheEsclef <108223871+TheEsclef@users.noreply.github.com>
2026-02-08 02:26:51 +0200

    Add files via upload

---
+++ WebIdleGame/main/main.js
@@ -0,0 +1,206 @@
+window.onload = function(){
+    // Mängija Omandid
+    var GP = 0;
+
+    // XP Seotud
+    var XP = 0;
+    var XPleft = 100;
+
+    // Upgradid
+    var SwordLvlBonus = 1;
+    var SwordLvlCost = 25;
+    var ShieldLvlBonus = 5;
+    var ShieldLvlCost = 20;
+    var BandageCost = 50;
+    var BandageLvlBonus = 1;
+
+    // Mängija statid
+    var PlayerDMG = PlayerLVL * SwordLvlBonus;
+    var PlayerMaxHP = (PlayerLVL * ShieldLvlBonus) + 15;
+    var PlayerHP = PlayerMaxHP;
+    var PlayerLVL = 1;
+    var SwordLvl = 1;
+    var ShieldLvl = 1;
+    var BandageLvl = 1;
+    var KillCount = 0;
+
+    // Tsoon
+    var ZoneLvl = 1;
+    var MinZoneLvl = (ZoneLvl + 1); 
+    var MaxZoneLvl = (ZoneLvl + 3);
+
+    // Vastasega Seotud
+    var EnemyNimi = "Vastast Pole";
+    var EnemyLvl = Math.floor(Math.random() * (MaxZoneLvl - MinZoneLvl + 1)) + MinZoneLvl;
+    var EnemyHP = 0;
+    var EnemyMaxHP = 0;
+    var EnemyDMG = 0;
+
+    // Vastase Loot
+    var EnemyGP = Math.floor(Math.random() * (ZoneLvl * EnemyLvl)) + 1;
+    var EnemyXP = Math.floor(Math.random() * ((ZoneLvl * EnemyLvl)) * 4) + 1;
+
+
+    function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    // SALVESTAB JA LAEB
+    const load = JSON.parse(localStorage.getItem("save"));
+    if (load) {PlayerHP = load.PlayerHP; PlayerMaxHP = load.PlayerMaxHP; PlayerDMG = load.PlayerDMG; GP = load.GP; XP = load.XP; XPleft = load.XPleft; ZoneLvl = load.ZoneLvl; }
+    if (load) {SwordLvlBonus = load.SwordLvlBonus; SwordLvlCost = load.SwordLvlCost; SwordLvl = load.SwordLvl; ShieldLvlCost = load.ShieldLvlCost; ShieldLvlBonus = load.ShieldLvlBonus; ShieldLvl = load.ShieldLvl}
+    if (load) {BandageLvl = load.BandageLvl; BandageLvlBonus = load.BandageLvlBonus; BandageCost = load.BandageCost, KillCount = load.KillCount}
+    if (load) {EnemyNimi = load.EnemyNimi; EnemyMaxHP = load.EnemyMaxHP; EnemyXP = load.EnemyXP; EnemyGP = load.EnemyGP; EnemyDMG = load.EnemyDMG; EnemyHP = load.EnemyHP; EnemyLvl = load.EnemyLvl;}
+
+    function SaveGame() {
+        const saveData = {PlayerHP, PlayerMaxHP, PlayerDMG, GP, XP, XPleft, ZoneLvl,
+            EnemyNimi, EnemyMaxHP, EnemyXP, EnemyGP, EnemyDMG, EnemyHP, EnemyLvl,
+            SwordLvl, ShieldLvl, SwordLvlBonus, SwordLvlCost, ShieldLvlBonus, ShieldLvlCost,
+            BandageLvl, BandageLvlBonus, BandageCost, KillCount}
+        localStorage.setItem("save", JSON.stringify(saveData)); 
+    }
+    
+    // KUSTUTAB SAVEI
+    this.document.getElementById("erasesave").onclick = function(){
+        localStorage.clear();
+        location.reload();
+    }
+
+    function UpdatePlayer(){
+        document.getElementById("PlayerDMG").innerHTML = PlayerDMG;
+        document.getElementById("PlayerHP").innerHTML = PlayerHP;
+        document.getElementById("PlayerMaxHP").innerHTML = PlayerMaxHP;
+        document.getElementById("PlayerLvl").innerHTML = PlayerLVL;
+        document.getElementById("GP").innerHTML = GP;
+        document.getElementById("XP").innerHTML = XP;
+        document.getElementById("XPleft").innerHTML = XPleft;
+        document.getElementById("SwordLvl").innerHTML = SwordLvl;
+        document.getElementById("ShieldLvl").innerHTML = ShieldLvl;
+        document.getElementById("BandageLvl").innerHTML = BandageLvl;
+        document.getElementById("BandageCost").innerHTML = BandageCost;
+        document.getElementById("BandageBonus").innerHTML = BandageLvlBonus;
+        document.getElementById("HealAmount").innerHTML = BandageLvl * PlayerLVL;
+        document.getElementById("KillCount").innerHTML = KillCount;
+    }
+
+    function UpdateEnemy(){
+        document.getElementById("EnemyNimi").innerHTML = EnemyNimi;
+        document.getElementById("EnemyLvl").innerHTML = EnemyLvl;
+        document.getElementById("EnemyHP").innerHTML = EnemyHP;
+        document.getElementById("EnemyMaxHP").innerHTML = EnemyMaxHP;
+        document.getElementById("EnemyDMG").innerHTML = EnemyDMG;
+        document.getElementById("EnemyGP").innerHTML = EnemyGP;
+        document.getElementById("EnemyXP").innerHTML = EnemyXP;
+    }
+
+    function UpdateStore(){
+        document.getElementById("SwordCost").innerHTML = SwordLvlCost;
+        document.getElementById("SwordBonus").innerHTML = SwordLvlBonus;
+        document.getElementById("ShieldCost").innerHTML = ShieldLvlCost;
+        document.getElementById("ShieldBonus").innerHTML = ShieldLvlBonus;
+        document.getElementById("BandageLvl").innerHTML = BandageLvl;
+        document.getElementById("BandageCost").innerHTML = BandageCost;
+        document.getElementById("BandageBonus").innerHTML = BandageLvlBonus;
+    }
+
+    async function Combat(){
+        if (EnemyNimi === "Täielik Tühjus"){
+            return;
+        }
+        EnemyHP = (EnemyHP - PlayerDMG);
+        PlayerHP = (PlayerHP - EnemyDMG);
+        UpdateEnemy();
+        UpdatePlayer();
+        if ((EnemyNimi === "Rott") && (EnemyHP > 0)){
+            document.getElementById("enemyimg").src = "./image/RatHurt.png";
+            await sleep(150);
+            document.getElementById("enemyimg").src = "./image/Rat.png";
+        }
+    }
+
+    // Põgenemine hetkesest kaklusest
+    async function Flee(){
+        EnemyNimi = "Täielik Tühjus";
+        EnemyLvl = 0;
+        EnemyMaxHP = 0;
+        EnemyHP = 0;
+        EnemyDMG = 0;
+        EnemyGP = 0;
+        EnemyXP = 0;
+
+        PlayerHP = PlayerMaxHP;
+        await sleep(200);
+        document.getElementById("enemyimg").src = "";
+    }
+
+    // Rotti sisse spawnimine
+    function SpawnRat(){
+        document.getElementById("enemyimg").src = "./image/Rat.png";
+        EnemyLvl = Math.floor(Math.random() * (MaxZoneLvl - MinZoneLvl + 1)) + MinZoneLvl;
+        EnemyNimi = "Rott";
+        EnemyMaxHP = ((EnemyLvl + ZoneLvl) * 8);
+        EnemyHP = EnemyMaxHP;
+        EnemyDMG = ((EnemyLvl + ZoneLvl) - 1);
+        EnemyGP = Math.floor(Math.random() * (ZoneLvl * EnemyLvl)) + 1;
+        EnemyXP = Math.floor(Math.random() * ((ZoneLvl * EnemyLvl)) * 4) + 1;
+    }
+    document.getElementById("attack").onclick = function(){
+        PlayerDMG = PlayerLVL * SwordLvlBonus;
+        PlayerMaxHP = (PlayerLVL * ShieldLvlBonus) + 15;
+        Combat();
+        // Vastane sureb
+        if (EnemyHP <= 0){
+            GP = EnemyGP + GP;
+            XP = EnemyXP + XP;
+            KillCount = KillCount + 1;
+            Flee();
+            if (XP >= XPleft){
+                XP = 0;
+                PlayerLVL = PlayerLVL + 1;
+                XPleft = PlayerLVL * 115;
+            }
+        }
+
+        // Mängija sureb
+        if (PlayerHP <= 0){
+            Flee();
+        }
+        UpdateEnemy();
+        UpdatePlayer();
+        SaveGame();
+    }
+    this.document.getElementById("flee").onclick = function(){
+        if (EnemyNimi === "Täielik Tühjus"){
+            return;
+            SaveGame();
+        }
+        Flee();
+        UpdateEnemy();
+        UpdatePlayer();
+        SaveGame();
+    }
+    this.document.getElementById("Heal").onclick =function(){
+        if (PlayerHP < PlayerMaxHP){
+            PlayerHP = PlayerHP + (BandageLvlBonus * PlayerLVL);
+            UpdatePlayer();
+        }
+    }
+    this.document.getElementById("Spawn").onclick = function(){
+        SpawnRat();
+        UpdateEnemy();
+        UpdatePlayer();
+    }
+
+    PlayerDMG = (PlayerLVL * SwordLvlBonus);
+    PlayerMaxHP = (PlayerLVL * ShieldLvlBonus) + 15;
+    PlayerHP = PlayerMaxHP;
+
+    if (!load) {
+        SpawnRat();
+    }
+    UpdatePlayer();
+    UpdateEnemy();
+    UpdateStore();
+    SaveGame();
+    if (EnemyNimi === "Rott"){document.getElementById("enemyimg").src = "./image/Rat.png";}
+}
